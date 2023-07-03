5c0e47cb6322f69a8610558134fba991c079921e
Zixuan Chen <zixuan.chen@ing.com>
2023-07-03 14:39:36 +0200

    init

---
+++ PracticeJava17TimeAPI/src/main/java/calendars/SchedulerCalendar.java
@@ -0,0 +1,45 @@
+package calendars;
+
+import java.util.Calendar;
+
+public class SchedulerCalendar extends Calendar {
+    @Override
+    protected void computeTime() {
+
+    }
+
+    @Override
+    protected void computeFields() {
+
+    }
+
+    @Override
+    public void add(int field, int amount) {
+
+    }
+
+    @Override
+    public void roll(int field, boolean up) {
+
+    }
+
+    @Override
+    public int getMinimum(int field) {
+        return 0;
+    }
+
+    @Override
+    public int getMaximum(int field) {
+        return 0;
+    }
+
+    @Override
+    public int getGreatestMinimum(int field) {
+        return 0;
+    }
+
+    @Override
+    public int getLeastMaximum(int field) {
+        return 0;
+    }
+}
